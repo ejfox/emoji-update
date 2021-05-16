8690916f6fa1094207bd22ebd6245c34f1c1c927
EJ Fox <ejfox@ejfox.com>
2021-05-16 16:22:47 -0400

    init

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'emoji-update',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/firebase',
+    {
+      config: {
+        apiKey: '<apiKey>',
+        authDomain: '<authDomain>',
+        projectId: '<projectId>',
+        storageBucket: '<storageBucket>',
+        messagingSenderId: '<messagingSenderId>',
+        appId: '<appId>',
+        measurementId: '<measurementId>'
+      },
+      services: {
+        auth: true, // Just as example. Can be any other service.
+        firestore: true
+      }
+    }
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
